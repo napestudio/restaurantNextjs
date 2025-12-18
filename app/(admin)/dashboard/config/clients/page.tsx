c40d02fb4560c62cc9f755a4ac7e802de2c3a0d3
Renzo Costarelli <renzo.costarelli@gmail.com>
2025-12-18 15:59:30 -0300

    fix warning y payment

--- app/(admin)/dashboard/config/clients/page.tsx
+++ app/(admin)/dashboard/config/clients/page.tsx
@@ -1,3 +1,13 @@
-export default function ClientsPage() {
-  return null;
+import { getClients } from "@/actions/clients";
+import { ClientsManager } from "@/components/dashboard/clients";
+import { BRANCH_ID } from "@/lib/constants";
+
+export default async function ClientsPage() {
+  const branchId = BRANCH_ID || "";
+
+  const clientsResult = await getClients(branchId);
+  const clients =
+    clientsResult.success && clientsResult.data ? clientsResult.data : [];
+
+  return <ClientsManager branchId={branchId} initialClients={clients} />;
 }
