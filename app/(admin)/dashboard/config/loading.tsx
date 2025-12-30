bad0b4a2cd562af3f6644b32d7fa2b54a00022b7
Renzo Costarelli <renzo.costarelli@gmail.com>
2025-12-30 15:35:42 -0300

    agrega loading

---
+++ app/(admin)/dashboard/config/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="text-2xl font-bold h-full w-full bg-white grid place-items-center">
+      <div className="animate-pulse">Cargando...</div>
+    </div>
+  );
+}
