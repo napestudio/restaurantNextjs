bbf8238216690079c9026018b90740d379be859e
Renzo Costarelli <renzo.costarelli@gmail.com>
2026-03-06 16:37:38 -0300

    Fix descripcion carta qr

--- components/carta/menu-item.tsx
+++ components/carta/menu-item.tsx
@@ -1,6 +1,7 @@
 import type { ProductTag } from "@/app/generated/prisma";
 import Image from "next/image";
 import { ProductTagIcons } from "../ui/product-tag-icons";
+import { formatCurrency } from "@/lib/currency";
 
 interface MenuItemProps {
   name: string;
@@ -34,8 +35,8 @@ export function MenuItem({
           />
         </div>
       )}
-      <div className="flex-1 flex flex-col min-h-full justify-between py-3">
-        <div className="flex  items-start justify-between gap-2">
+      <div className="flex-1 flex flex-col min-h-full justify-between pb-3">
+        <div className="flex flex-col md:flex-row items-start justify-between gap-2">
           <h3 className="font-semibold text-lg text-white">
             <div className="flex md:flex-row flex-col gap-1 items-center">
               {name}
@@ -52,20 +53,18 @@ export function MenuItem({
               </span>
             )} */}
           </h3>
-          <div className="flex-1 h-px w-full bg-white/50 mb-1.5 self-end"></div>
+          <div className="hidden md:block flex-1 h-px w-full bg-white/50 mb-1.5 self-end"></div>
           {price !== null &&
             price !== undefined &&
             price !== 0 &&
             showPrice && (
-              <span className="font-bold text-lg text-white shrink-0">
-                ${price}
-              </span>
+              <div className="font-bold text-sm md:text-lg text-white block">
+                {formatCurrency(price)}
+              </div>
             )}
         </div>
         {description && (
-          <p className="text-neutral-400 text-sm mt-1 line-clamp-2">
-            {description}
-          </p>
+          <p className="text-neutral-400 text-sm mt-1">{description}</p>
         )}
         {tags.length > 0 && (
           <div className="mt-1.5 block md:hidden">
