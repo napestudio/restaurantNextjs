f0f1a8e36d3ef69afef68381f8d9b5512000a27f
Renzo Costarelli <renzo.costarelli@gmail.com>
2026-03-10 17:25:52 -0300

    fix deploy

--- actions/Table.ts
+++ actions/Table.ts
@@ -1048,7 +1048,7 @@ export async function updateTableFloorPlan(
     height?: number;
     rotation?: number;
     shape?: "SQUARE" | "RECTANGLE" | "CIRCLE";
-    status?: "EMPTY" | "OCCUPIED" | "RESERVED" | "CLEANING";
+    status?: "EMPTY" | "OCCUPIED" | "RESERVED" | "CLEANING" | "PAYING";
   }
 ) {
   try {
